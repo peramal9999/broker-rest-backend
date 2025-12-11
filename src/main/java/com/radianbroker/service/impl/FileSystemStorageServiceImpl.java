9abdb99a0e472ff07eb29b23085ff1376af487f1
Kavita.Dumbare <kavita.dumbare@peramalservices.com>
2025-12-11 13:42:31 +0530

    updated the api for fetch hl7 received message

--- src/main/java/com/radianbroker/service/impl/FileSystemStorageServiceImpl.java
+++ src/main/java/com/radianbroker/service/impl/FileSystemStorageServiceImpl.java
@@ -2,20 +2,39 @@ package com.radianbroker.service.impl;
 
 import java.net.MalformedURLException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
+import com.radianbroker.config.StorageProperties;
 import com.radianbroker.exceptions.StorageException;
 import com.radianbroker.service.FileSystemStorageService;
+
 
 @Service
 public class FileSystemStorageServiceImpl implements FileSystemStorageService{
 	
-	private final Path rootLocation=null;
+	private final Path rootLocation;
 	
+	private String storageAlias;
 	
+	@Autowired
+	public FileSystemStorageServiceImpl(StorageProperties properties) {
+		this.storageAlias = properties.getAlias();
+		String OS = System.getProperty("os.name").toLowerCase();
+		if(OS.equals("win") || OS.startsWith("windows")) {
+			this.rootLocation = Paths.get(properties.getLocation());
+		}else if(OS.equals("mac") || OS.equals("linux")) {
+//			String homeDir = System.getProperty("user.home") + properties.getLocation();
+//			this.rootLocation = Paths.get(homeDir); 
+			this.rootLocation = Paths.get(properties.getLocation());
+		} else {
+			throw new StorageException("OS not supported: " + OS);
+		}
+	}
 	@Override
 	public Resource loadAsResource(String documentPath)throws Exception {
 		try {
